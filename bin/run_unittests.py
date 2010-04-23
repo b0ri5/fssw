8a0b7cfe614a9d831d9b56fc7df7f7833da1785c
Greg Tener <Greg Tener@localhost>
2010-04-23 05:47:58 +0300

    made build scripts more agnostic

--- bin/run_unittests.py
+++ bin/run_unittests.py
@@ -16,19 +16,19 @@ def main():
   
   options, args = parser.parse_args()
   
-  dir = os.path.dirname(__file__)
+  bindir = os.path.dirname(__file__)
   
   # run the tests ending in "-db"
   if options.debug:
-    for f in os.listdir(os.path.join(dir, 'test') ):
+    for f in os.listdir(os.path.join(bindir, 'test') ):
       if f.endswith('-db'):
-        os.system(os.path.join(dir, 'test', f) )
+        os.system(os.path.join(bindir, 'test', f) )
   
   # run the tests not ending with "-db"
   if options.release:
-    for f in os.listdir(os.path.join(dir, 'test') ):
+    for f in os.listdir(os.path.join(bindir, 'test') ):
       if not f.endswith('-db'):
-        os.system(os.path.join(dir, 'test', f) )
+        os.system(os.path.join(bindir, 'test', f) )
       
 if __name__ == "__main__":
   main()
