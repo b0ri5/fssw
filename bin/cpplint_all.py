f7642af98e01b9beedc03adee3c437ab3ae8ab4c
Greg Tener <Greg Tener@localhost>
2010-04-26 14:37:17 -0400

    resolved the TODO in cpplint_all.py to suppress the output of files with no warnings

--- bin/cpplint_all.py
+++ bin/cpplint_all.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import subprocess as sp
 
 src_dirs = [os.path.join('include', 'fssw'), 'src', 'test']
 
@@ -13,13 +14,16 @@ def main():
   for src_dir in src_dirs:
     
     for src_file in os.listdir(os.path.join(root, src_dir) ):
+      # only process C++ files
       if os.path.splitext(src_file)[1] in ['.cc', '.h']:
-        cmd = '%s %s %s %s' % \
-          (sys.executable, cpplint,
-           '--filter=-runtime/virtual',
-           os.path.join(root, src_dir, src_file) )
-        os.system(cmd)
-        # TODO: supress output of files for which no errors are found
-      
+        cmd = [sys.executable, cpplint, '--filter=-runtime/virtual',
+           os.path.join(root, src_dir, src_file) ]
+          
+        p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+        out, err = p.communicate()
+        
+        if not err.strip().endswith('Total errors found: 0'):
+          print err
+        
 if __name__ == "__main__":
   main()
